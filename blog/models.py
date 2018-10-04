217c828ae3b2d1cc4d901ae2c8ea36f6a02274dd
xtine <christine@fieryorange.com>
2018-10-03 23:56:28 -0700

    models for blog post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, blank=False)
+    body = models.TextField(blank=False)
+    image = models.ImageField(upload_to='media/', blank=True)
+    date_edited = models.DateTimeField(default=timezone.now)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
